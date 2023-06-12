6a9ba18914ecc772f4903ffd975e58716a125856
Sagar Kasundra <132868232+sagar9322@users.noreply.github.com>
2023-06-12 08:47:05 +0530

    Delete util directory

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('candy-shop', 'root', '*********', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
